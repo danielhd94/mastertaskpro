b56e94b4db43fcd0410a47f16538be86caa070b7
Daniel Hernandez <danielhd94@hotmail.com>
2025-08-06 09:35:26 -0700

    add setUsername test

--- src/app/services/app/test/app-state.spec.ts
+++ src/app/services/app/test/app-state.spec.ts
@@ -1,1 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { AppStateService } from '../app-state.service';
+
+describe('AppStateService', () => {
+  let service: AppStateService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AppStateService],
+    });
+    service = TestBed.inject(AppStateService);
+  });
+
+  it('Servicio de estado de la aplicacion creado', () => {
+    expect(service).toBeTruthy();
+  });
+
+
+  describe('setUsername', () => {
+    it('deberia establecer el nombre de usuario correctamente', () => {
+      // Arrange: Iniciar sesion - Preparar datos de prueba
+      const username = 'admin';
+
+      // Act: Iniciar sesion - Ejecutar la funcion
+      service.setUsername(username);
+
+      // Assert: Verificar el resultado - Verificar el estado de la autenticacion
+      expect(service.username()).toEqual(username);
+    });
+  });
+
+});
